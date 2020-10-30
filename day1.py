8f33713c443baebde7e79a9e0128af40985b8a6b
Rishabh Tyagi <rishabh.dev.tyagi@gmail.com>
2020-10-30 17:22:48 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,20 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+int_input = int(input())
+double_input = float(input())
+string_input = str(input())
+
+
+
+
+# Print the sum of both integer variables on a new line.
+print(i + int_input)
+# Print the sum of the double variables on a new line.
+print(d + double_input)
+# Concatenate and print the String variables on a new line
+print(s + string_input)
+# The 's' variable above should be printed first.
